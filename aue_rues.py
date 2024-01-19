b65415fbcfd9e648f22f691de0f82565341eb660
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-01-19 09:17:51 +0100

    Add aue_rues

---
+++ aue_rues.py
@@ -0,0 +1,44 @@
+"""
+# aue_rues
+This DAG updates the following datasets:
+
+- [100046](https://data.bs.ch/explore/dataset/100046)
+- [100323](https://data.bs.ch/explore/dataset/100323)
+"""
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+from airflow.operators.docker_operator import DockerOperator
+
+default_args = {
+    'owner'                 : 'jonas.bieri',
+    'description'           : 'Run the aue_rues docker container',
+    'depend_on_past'        : False,
+    'start_date'            : datetime(2024, 1, 19),
+    'email'                 : ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch", "orhan.saeedi@bs.ch", "nicolas.maire@bs.ch"],
+    'email_on_failure'      : True,
+    'email_on_retry'        : False,
+    'retries'               : 0,
+    'retry_delay'           : timedelta(minutes=15)
+}
+
+with DAG('aue_rues', default_args=default_args, schedule_interval="*/10 * * * *", catchup=False) as dag:
+    dag.doc_md = __doc__
+    upload_bag_datasets = DockerOperator(
+        task_id='upload',
+        image='aue_rues:latest',
+        api_version='auto',
+        auto_remove=True,
+        command='python3 -m aue_rues.etl',
+        container_name='aue_rues--upload',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[{
+            "Source": "/data/dev/workspace/data-processing",
+            "Target": "/code/data-processing",
+            "Type": "bind",
+            "ReadOnly": False
+        }]
+    )
+
