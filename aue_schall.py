d8aa57521cdafb67206f700fb8da22902f661e85
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-01-19 10:39:02 +0100

    Add aue_schall

---
+++ aue_schall.py
@@ -0,0 +1,42 @@
+"""
+# aue_schall
+This DAG updates the following datasets:
+
+- [100087](https://data.bs.ch/explore/dataset/100087)
+"""
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+from airflow.operators.docker_operator import DockerOperator
+
+default_args = {
+        'owner'                 : 'jonas.bieri',
+        'description'           : 'Run the aue_schall docker container',
+        'depend_on_past'        : False,
+        'start_date'            : datetime(2024, 1, 19),
+        'email'                 : ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch", "orhan.saeedi@bs.ch", "nicolas.maire@bs.ch"],
+        'email_on_failure'      : True,
+        'email_on_retry'        : False,
+        'retries'               : 0,
+        'retry_delay'           : timedelta(minutes=15)
+}
+
+with DAG('aue_schall', default_args=default_args, schedule_interval="*/15 * * * *", catchup=False) as dag:
+        dag.doc_md = __doc__
+        upload = DockerOperator(
+                task_id='upload',
+                image='aue_schall:latest',
+                api_version='auto',
+                auto_remove=True,
+                command='python3 -m aue_schall.etl',
+                container_name='aue_schall',
+                docker_url="unix://var/run/docker.sock",
+                network_mode="bridge",
+                tty=True,
+                mounts=[{
+                        "Source": "/data/dev/workspace/data-processing",
+                        "Target": "/code/data-processing",
+                        "Type": "bind",
+                        "ReadOnly": False
+                }]
+        )
