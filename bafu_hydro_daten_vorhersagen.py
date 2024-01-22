5992910cc05b51923743eca20e25ecf42e6df621
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-01-22 08:54:54 +0100

    Add bafu_hydro_daten_vorhersagen

---
+++ bafu_hydro_daten_vorhersagen.py
@@ -0,0 +1,43 @@
+"""
+# bafu_hydro_daten
+This DAG updates the following datasets:
+
+- [100271](https://data.bs.ch/explore/dataset/100271)
+- [100272](https://data.bs.ch/explore/dataset/100272)
+"""
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+from airflow.operators.docker_operator import DockerOperator
+
+default_args = {
+    'owner': 'jonas.bieri',
+    'description': 'Run the bafu_hydrodaten_vorhersagen docker container',
+    'depend_on_past': False,
+    'start_date': datetime(2024, 1, 22),
+    'email': ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch", "orhan.saeedi@bs.ch", "nicolas.maire@bs.ch"],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=3)
+}
+
+with DAG('bafu_hydrodaten_vorhersagen', default_args=default_args, schedule_interval="0 * * * *", catchup=False) as dag:
+    dag.doc_md = __doc__
+    upload = DockerOperator(
+        task_id='upload',
+        image='bafu_hydrodaten_vorhersagen:latest',
+        api_version='auto',
+        auto_remove=True,
+        command='python3 -m bafu_hydrodaten_vorhersagen.etl',
+        container_name='bafu_hydrodaten_vorhersagen',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[{
+            "Source": "/data/dev/workspace/data-processing",
+            "Target": "/code/data-processing",
+            "Type": "bind",
+            "ReadOnly": False
+        }]
+    )
