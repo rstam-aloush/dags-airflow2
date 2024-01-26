e3f9d46321db11cb71c11781ecaf150ee82287c0
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-01-26 16:01:16 +0100

    Add euroairport.py

---
+++ euroairport.py
@@ -0,0 +1,54 @@
+"""
+# euroairport
+This DAG updates the following datasets:
+
+- [100078](https://data.bs.ch/explore/dataset/100078)
+"""
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+
+default_args = {
+    'owner': 'jonas.bieri',
+    'description': 'Run the euroairport docker container',
+    'depend_on_past': False,
+    'start_date': datetime(2024, 1, 26),
+    'email': ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch", "orhan.saeedi@bs.ch", "nicolas.maire@bs.ch"],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=30)
+}
+
+with DAG('euroairport', default_args=default_args, schedule_interval="0 8 * * *", catchup=False) as dag:
+    dag.doc_md = __doc__
+    upload = DockerOperator(
+        task_id='upload',
+        image='euroairport:latest',
+        api_version='auto',
+        auto_remove='force',
+        command='/bin/bash /code/data-processing/euroairport/etl.sh ',
+        container_name='euroairport',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind"),
+                Mount(source="/mnt/OGD-DataExch/EuroAirport", target="/code/data-processing/euroairport/data",
+                      type="bind")]
+    )
+
+    ods_publish = DockerOperator(
+        task_id='ods-publish',
+        image='ods-publish:latest',
+        api_version='auto',
+        auto_remove='force',
+        command='python3 -m ods_publish.etl_id 100078',
+        container_name='euroairport--ods-publish',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
+    )
+
+    upload >> ods_publish
