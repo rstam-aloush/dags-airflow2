5a7f5df0f806e93d3aba5c85ae71066b1f42a482
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-01-29 17:18:01 +0100

    Change date to today

--- staka_abstimmungen.py
+++ staka_abstimmungen.py
@@ -15,7 +15,7 @@ default_args = {
     'owner': 'jonas.bieri',
     'description': 'Run the staka_abstimmungen docker container',
     'depend_on_past': False,
-    'start_date': datetime(2022, 2, 8),
+    'start_date': datetime(2024, 1, 29),
     'email': ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch", "orhan.saeedi@bs.ch", "nicolas.maire@bs.ch"],
     'email_on_failure': True,
     'email_on_retry': False,
