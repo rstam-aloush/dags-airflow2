a1ba4117d973746ee12ba0f2493e7687642a0045
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-02-02 14:08:06 +0100

    Add parkendd.py

---
+++ parkendd.py
@@ -0,0 +1,39 @@
+"""
+# parkendd
+This DAG updates the following datasets:
+
+- [100014](https://data.bs.ch/explore/dataset/100014)
+- [100044](https://data.bs.ch/explore/dataset/100044)
+"""
+
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+
+default_args = {
+    'owner': 'jonas.bieri',
+    'description': 'Run the parkendd docker container',
+    'depend_on_past': False,
+    'start_date': datetime(2024, 2, 2),
+    'email': ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch", "orhan.saeedi@bs.ch", "nicolas.maire@bs.ch"],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=3)
+}
+
+with DAG('parkendd', default_args=default_args, schedule_interval="0 * * * *", catchup=False) as dag:
+    dag.doc_md = __doc__
+    process_upload = DockerOperator(
+        task_id='process-upload',
+        image='parkendd:latest',
+        api_version='auto',
+        auto_remove='force',
+        command='/bin/bash /code/data-processing/parkendd/etl.sh ',
+        container_name='parkendd',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
+    )
