d105bc478e1c3494fd4c555eb69ce146f725d84a
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-02-02 13:54:15 +0100

    Add ods_catalog.py

---
+++ ods_catalog.py
@@ -0,0 +1,39 @@
+"""
+# ods_catalog
+This DAG updates the following datasets:
+
+- [100057](https://data.bs.ch/explore/dataset/100057)
+"""
+
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+
+default_args = {
+    'owner': 'jonas.bieri',
+    'description': 'Run the ods_catalog docker container',
+    'depend_on_past': False,
+    'start_date': datetime(2024, 2, 2),
+    'email': ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch", "orhan.saeedi@bs.ch", "nicolas.maire@bs.ch"],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=15)
+}
+
+
+with DAG('ods_catalog', default_args=default_args, schedule_interval='3 * * * *', catchup=False) as dag:
+    dag.doc_md = __doc__
+    upload = DockerOperator(
+        task_id='upload',
+        image='ods_catalog:latest',
+        api_version='auto',
+        auto_remove='force',
+        command='python3 -m ods_catalog.etl',
+        container_name='ods_catalog',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
+    )
