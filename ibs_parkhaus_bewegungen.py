7066c00b324f7d76e78460edfc550dae26c07825
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-03-06 14:52:34 +0100

    Add parkaus_bewegungen

---
+++ ibs_parkhaus_bewegungen.py
@@ -0,0 +1,41 @@
+"""
+# ibs_parkhaus_bewegungen.py
+This DAG updates the following datasets:
+
+- [100198](https://data.bs.ch/explore/dataset/100198)
+
+"""
+
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+
+default_args = {
+    'owner': 'hester.pieters',
+    'description': 'Run the ibs_parkhaus_bewegungen.py docker container',
+    'depend_on_past': False,
+    'start_date': datetime(2024, 2, 2),
+    'email': ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch", "orhan.saeedi@bs.ch", "nicolas.maire@bs.ch"],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=15)
+}
+
+with DAG('ibs_parkhaus_bewegungen', default_args=default_args, schedule_interval=None, catchup=False) as dag:
+    dag.doc_md = __doc__
+    upload = DockerOperator(
+        task_id='upload',
+        image='ibs_parkhaus_bewegungen:latest',
+        api_version='auto',
+        auto_remove='force',
+        command='python3 -m ibs_parkhaus_bewegungen.etl',
+        container_name='ibs_parkhaus_bewegungen--upload',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind"),
+                Mount(source="/mnt/OGD-DataExch/FD-immobilienbs/IBS_Parkhaus_Bewegungen",
+                      target="/code/data-processing/ibs_parkhaus_bewegungen/data_orig", type="bind")]
+    )
