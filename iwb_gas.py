699ba50e60cc5180139d7cc59c1206431070bcd0
Orhan Saeedi <orhan.saeedi@bs.ch>
2024-03-07 09:50:43 +0100

    Add fit_model as job

--- iwb_gas.py
+++ iwb_gas.py
@@ -3,6 +3,7 @@
 This DAG updates the following datasets:
 
 - [100304](https://data.bs.ch/explore/dataset/100304)
+- [100358](https://data.bs.ch/explore/dataset/100358)
 
 """
 from airflow import DAG
@@ -11,7 +12,7 @@ from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
 default_args = {
-    'owner': 'hester.pieters',
+    'owner': 'orhan.saeedi',
     'description': 'Run the iwb_gas.py docker container',
     'depend_on_past': False,
     'start_date': datetime(2024, 1, 26),
@@ -36,3 +37,20 @@ with DAG('iwb_gas', default_args=default_args, schedule_interval="0 13 * * *", c
         tty=True,
         mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
     )
+
+    fit_model = DockerOperator(
+        task_id='fit_model',
+        image='gasverbrauch:latest',
+        api_version='auto',
+        auto_remove='force',
+        command='Rscript /code/data-processing/stata_erwarteter_gasverbrauch/Gasverbrauch_OGD.R',
+        container_name='gasverbrauch--fit_model',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind"),
+                Mount(source="/mnt/OGD-DataExch/StatA/Gasverbrauch",
+                      target="/code/data-processing/stata_erwarteter_gasverbrauch/data/export", type="bind")]
+    )
+
+    upload >> fit_model
