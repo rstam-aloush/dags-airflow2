0fbb890e4ea833c24b22bdc8f7fa5db39d1e1568
Renato Farruggio <renato.farruggio@bluewin.ch>
2024-12-10 17:02:18 +0100

    Create airflow job for parkhaeuser

---
+++ parkhaeuser.py
@@ -0,0 +1,39 @@
+"""
+# parkhaeuser
+This DAG updates the following datasets:
+
+- [100088](https://data.bs.ch/explore/dataset/100088)
+"""
+
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+
+default_args = {
+    'owner': 'renato.farruggio',
+    'description': 'Run the parkhaeuser docker container',
+    'depend_on_past': False,
+    'start_date': datetime(2024, 10, 12),
+    'email': ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch", "orhan.saeedi@bs.ch",
+              "nicolas.maire@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=3)
+}
+
+with DAG('parkhaeuser', default_args=default_args, schedule_interval="* * * * *", catchup=False) as dag:
+    dag.doc_md = __doc__
+    process_upload = DockerOperator(
+        task_id='process-upload',
+        image='parkhaeuser:latest',
+        api_version='auto',
+        auto_remove='force',
+        command='python3 -m parkhaeuser.etl',
+        container_name='parkhaeuser',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
+    )
